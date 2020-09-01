8ab0fc1e9dd465c52330b9a3a936af03c413c475
aTNa <atay.narynov@gmail.com>
2020-09-01 18:16:25 +0600

    Updates

---
+++ precache-manifest.3eef313534ed38a8ca08c9f106e05870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb4dbdce2ca1bc227f08927fd4397ed",
+    "url": "/kloop-api/index.html"
+  },
+  {
+    "revision": "8d8d52b42b00e341dd1c",
+    "url": "/kloop-api/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "94909e48a02c9e7aa94b",
+    "url": "/kloop-api/static/js/2.cb8eaa1b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kloop-api/static/js/2.cb8eaa1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8d52b42b00e341dd1c",
+    "url": "/kloop-api/static/js/main.ca0460ac.chunk.js"
+  },
+  {
+    "revision": "cb142fd8077babd02341",
+    "url": "/kloop-api/static/js/runtime-main.c25a98e1.js"
+  }
+]);
