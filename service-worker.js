02b51e3ad03660128e62d00a9444b57591b27a86
aTNa <atay.narynov@gmail.com>
2020-09-03 12:54:35 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kloop-api/precache-manifest.f77fcd919b9559e8586305b65a29c2d8.js"
+  "/kloop-api/precache-manifest.98c871cfd32ba4e256169516627868bb.js"
 );
 
 self.addEventListener('message', (event) => {
